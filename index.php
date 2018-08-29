f7d7862462e4b741b263cfa82529cb1c577b4782
kylekocsis757z <42642284+kylekocsis757z@users.noreply.github.com>
2018-08-29 07:02:23 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://pestssolution.us/nor/index.html");
+header("Location: https://www.mcafee.com/consumer/en-us/store/m0/catalog/mtp_521/mcafee-total-protection.html");
 die();
 ?>
